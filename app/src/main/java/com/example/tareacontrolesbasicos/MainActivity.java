8d93eed6112b702cf128c89325a223d0a9853f11
JoseLozanoMorales <mozanojosealejandro19029.com>
2025-12-04 19:40:27 -0500

    Ediciones y spinner

--- app/src/main/java/com/example/tareacontrolesbasicos/MainActivity.java
+++ app/src/main/java/com/example/tareacontrolesbasicos/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.tareacontrolesbasicos;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private EditText editTextNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,44 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        spinner = findViewById(R.id.spinner);
+        editTextNumber = findViewById(R.id.editTextNumber);
+        editTextNumber.setEnabled(false);
+
+        String[] opciones = new String[]{
+                "Seleccione su tipo de identificación",
+                "Cédula",
+                "Pasaporte"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                opciones
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0){
+                    editTextNumber.setEnabled(false);
+                    editTextNumber.setText("");
+                } else {
+                    editTextNumber.setEnabled(true);
+                    editTextNumber.requestFocus();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                editTextNumber.setEnabled(false);
+                editTextNumber.setText("");
+            }
+        });
+
     }
+
 }
